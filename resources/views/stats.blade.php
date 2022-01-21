fbcab96b1b40802f0b5f2dfe470d6e6449fdb392
Schmidt Gergő <s.gergo.az@gmail.com>
2022-01-21 15:00:11 +0100

    routes init

--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -11,7 +11,7 @@
     <p>Total number of feladatok: {{ $feladatcount }}</p>
     <p>Total number of ertekeletlen feladatok: {{ $feladatertekelve }}</p>
     <!--<p>
-       <a href="{{ route('statues.index') }}">Go to statue listing</a>
+       <a href="{{ route('feladatok.index') }}">Go to statue listing</a>
     </p>-->
 </body>
 </html>
